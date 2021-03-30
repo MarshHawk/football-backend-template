ee819e7b35000a65b36441aa4b126b5c96b84052
Sean Glover <s.glover12@gmail.com>
2021-03-30 09:40:25 -0400

    basic concept

--- lib/mls-football-app-stack.ts
+++ lib/mls-football-app-stack.ts
@@ -1,18 +1,87 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import { AttributeType, BillingMode, Table } from '@aws-cdk/aws-dynamodb';
+import { CfnApiKey, CfnDataSource, CfnGraphQLApi, CfnGraphQLSchema, IamResource } from '@aws-cdk/aws-appsync';
+import { ManagedPolicy, Role, ServicePrincipal } from '@aws-cdk/aws-iam';
 
 export class MlsFootballAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'MlsFootballAppQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    //### Dynamo Tables
+    const playersTableName = 'player';
+    const gamesTableName = 'Games';
+
+    const playersTable = new Table(this, 'MlsFootballPlayers', {
+      tableName: playersTableName,
+      partitionKey: {
+        name: playersTableName + 'Id',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
-    const topic = new sns.Topic(this, 'MlsFootballAppTopic');
+    const gamesTable = new Table(this, 'MlsFootballGames', {
+      tableName: gamesTableName,
+      partitionKey: {
+        name: gamesTableName + 'Id',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // CFN Api
+    const footballGraphQLApi = new CfnGraphQLApi(this, 'FootballApi', {
+      name: 'football-api',
+      authenticationType: 'API_KEY',
+    });
+
+    new CfnApiKey(this, 'FootballApiKey', {
+      apiId: footballGraphQLApi.attrApiId
+    });
+
+    const apiSchema = new CfnGraphQLSchema(this, 'ItemsSchema', {
+      apiId: footballGraphQLApi.attrApiId,
+      definition: `type ${playersTableName} {
+        ${playersTableName}Id: ID!
+        name: String
+        phone: String
+      }
+      type Paginated${playersTableName} {
+        items: [${playersTableName}!]!
+        nextToken: String
+      }
+      type Query {
+        allPlayers(limit: Int, nextToken: String): Paginated${playersTableName}!
+        getOnePlayer(${playersTableName}Id: ID!): ${playersTableName}
+      }
+      type Mutation {
+        savePlayer(name: String!): ${playersTableName}
+        deletePlayer(${playersTableName}Id: ID!): ${playersTableName}
+      }
+      type Schema {
+        query: Query
+        mutation: Mutation
+      }`
+    });
+
+    const footballRole = new Role(this, 'ItemsDynamoDBRole', {
+      assumedBy: new ServicePrincipal('appsync.amazonaws.com')
+    });
+
+    footballRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'));
+
+    const dataSource = new CfnDataSource(this, 'PlayersDataSource', {
+      apiId: footballGraphQLApi.attrApiId,
+      name: 'PlayersDynamoDataSource',
+      type: 'AMAZON_DYNAMODB',
+      dynamoDbConfig: {
+        tableName: playersTable.tableName,
+        awsRegion: this.region
+      },
+      serviceRoleArn: footballRole.roleArn
+    });
+
   }
 }
